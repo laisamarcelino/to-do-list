ce065613a38f756b66c22bf933c364e74d984da8
laisamarcelino <laisamsr12@gmail.com>
2023-10-10 14:31:06 -0300

    finalização do estilo do componente

--- src/components/GeneralTasks/generalTasks.styles.js
+++ src/components/GeneralTasks/generalTasks.styles.js
@@ -5,22 +5,62 @@ export const StylesGeneralTasks = styled.section`
     margin: 5%;
 
     h1 {
-        text-align: center ;
-        border-bottom: 2px solid #000000;
-        padding: 20px;
+        text-align: center;
+        border-bottom: 3px solid  ${(props) => props.theme.gray};
+        padding: 25px;
         margin-bottom: 50px;
+        color: ${(props) => props.theme.blackLight};
     }
-    
-    .section {
+
+    section, .icon-align {
         display: flex;
         flex-direction: row;
+        justify-content: space-around;
     }
 
-    .column {
-        border: 2px solid;
+    .task-align {
+        width: 100%;
+    }
+
+    .icon-align {
         width: fit-content;
-        
+        gap: 10px;
+        margin-bottom: 20px;
     }
 
+    .h2-1 {
+        color: ${(props) => props.theme.gray};
+    }
+  
+    .h2-2 {
+        color: ${(props) => props.theme.greenLight};
+    }
+
+    .h2-3 {
+        color: ${(props) => props.theme.jade};
+    }
+
+    .add-button, .task {
+        border: 2px black solid;
+        border-radius: 5px;
+        padding: 10px;
+        font-size: 15px;
+        margin-bottom: 5%;
+        font-family: 'Montserrat', sans-serif;
+        background-color: transparent;
+        width: 80%;
+    }
+
+    .task {
+        position: relative;
+    }
+
+    .more-button {
+        background-color: transparent;
+        border: none;
+        position: absolute;
+        top: 4px;
+        right: 16px;
+    }
 
 `
