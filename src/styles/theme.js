1a24c44859dd08c875054c02e3a71fc9f7854cef
laisamarcelino <laisamsr12@gmail.com>
2023-10-10 14:13:32 -0300

    adicao de novas cores

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,7 +3,10 @@ export const ColorTheme = {
     black: '#000000',
     orange: '#FF8811',
     jade: '#00A676',
-    purple: '#9395D3'
+    purple: '#9395D3',
+    gray: '#5B5555',
+    greenLight: '#81DB9E',
+    blackLight: '#231F20'
 }
 
 //it's a object
