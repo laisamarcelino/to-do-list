dd595f9c637a0b54bcc8862ed1f3103afe706cc3
laisamarcelino <laisamsr12@gmail.com>
2023-10-12 00:11:26 -0300

    criacao da pagina addtask'

--- src/pages/AddTask/AddTask.jsx
+++ src/pages/AddTask/AddTask.jsx
@@ -1,27 +1,59 @@
 import React from "react";
-// import { StylesAddTask } from "./";
+import { StylesAddTask } from "./addTask.styles";
 import { Link, useNavigate } from "react-router-dom";
+import Button from "../../components/common/Button/Button";
 
 const AddTask = () => {
+
+  const navigate = useNavigate();
+
+  const ButtonReturn = () => {
+    navigate("/");
+  };
+
   return (
-    // <StylesAddTask>
-    <form action="">
-      <Textfield
-        nome="nome"
-        label="none"
-        type="text"
-        required
-        placeholder="maria"
-        value={nome}
-        onChange={(e) => setNome(e)}
-      />
-{/*       
-      <input type="text" />
-      <input type="date" />
-      <input type="time" />
-      <textarea cols="30" rows="10"></textarea> */}
-    </form>
-    // </StylesAddTask>
+    <StylesAddTask>
+      <div className="title">
+        <form action="">
+          <input
+            type="text"
+            className="title-input"
+            placeholder="        Título da Tarefa"
+          />
+        </form>
+      </div>
+
+      <section>
+        <form action="">
+          <div className="align-datetime">
+            <div className="align-text-input">
+              <label htmlFor="date"> Data: </label>
+              <input type="date" className="input-datetime" />
+            </div>
+
+            <div className="align-text-input">
+              <label htmlFor="time"> Horário: </label>
+              <input type="time" className="input-datetime" />
+            </div>
+          </div>
+
+          <div className="align-description">
+            <label htmlFor="description"> Descrição: </label>
+            <textarea
+              placeholder="Digite algo..."
+              className="description"
+              cols="30"
+              rows="10"
+            ></textarea>
+          </div>
+        </form>
+      </section>
+
+      <div className="align-button">
+        <Button texto="Salvar" variant="primary" width="20%" height="55px" />
+        <Button texto="Cancelar" variant="secondary" width="20%" height="55px" onClick={ButtonReturn}/>
+      </div>
+    </StylesAddTask>
   );
 };
 
--- src/pages/AddTask/addTask.styles.js
+++ src/pages/AddTask/addTask.styles.js
@@ -1,6 +1,69 @@
 import styled from "styled-components"
 
 export const StylesAddTask = styled.main`
-    background-color: blue;
+    
+    margin: 5% 10%;
+    border: solid black 2px;
+    border-radius: 12px;
+    background-color: ${(props) => props.theme.grayLight};
+    padding: 30px;
+
+    .title {
+        display: flex;
+        justify-content: center;
+        border-bottom: 3px solid  ${(props) => props.theme.gray};
+        padding: 25px;
+        margin-bottom: 50px;
+    }
+    
+    .title-input {
+        border: none;
+        font-family: 'Montserrat', sans-serif;
+        font-size: 36px;
+        color: ${(props) => props.theme.black};
+        font: bold;
+        background-color: transparent;
+    }
+
+    .align-datetime {
+        display: flex;
+        justify-content: space-around;
+        margin-bottom: 40px;
+    }
+
+    .align-text-input {
+        display: flex;
+        gap: 20px;
+        align-items: center;
+    }
+
+    label {
+        font-size: 20px;
+    }
+
+    .align-description {
+        display: flex;
+        gap: 20px;
+        flex-direction: column;
+    }
+
+    .input-datetime, .description {
+        border-radius: 20px;
+        padding: 15px;
+        font-size: 20px;
+        font-family: 'Montserrat', sans-serif;
+    }
+
+    .input-datetime {
+        background-color: ${(props) => props.theme.greenLight};
+        border-color: ${(props) => props.theme.jade};
+    }
+
+    .align-button {
+        margin-top: 40px;
+        display: flex;
+        justify-content: center;
+        gap: 20px;
+    }
 `
     
