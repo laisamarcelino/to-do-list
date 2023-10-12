1a2055f756026ea95a0cbe31b47998c99776bb0b
laisamarcelino <laisamsr12@gmail.com>
2023-10-12 17:21:30 -0300

    update: atualização das rotas

--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -1,27 +1,28 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import AddTask from './pages/AddTask/AddTask'
-import LandingPage from './pages/LandingPage/LandingPage'
-import { GlobalStyle } from './styles/GlobalStyles'
-import { ColorTheme } from './styles/theme'
-import { ThemeProvider } from 'styled-components'
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AddTask from "./components/views/AddTask/AddTask";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import { GlobalStyle } from "./styles/GlobalStyles";
+import { ColorTheme } from "./styles/theme";
+import { ThemeProvider } from "styled-components";
+import GeneralTasks from "./pages/GeneralTasks/GeneralTasks";
+import { TaskProvider } from "./pages/TaskContent/TaskContent";
 
 const Rotas = () => {
   return (
-
     <BrowserRouter>
       <ThemeProvider theme={ColorTheme}>
         <GlobalStyle />
-        <Routes>
-          <Route path='/AddTask' element={<AddTask />} />
-          <Route path='/' element={<LandingPage />} />
-        </Routes>
-
+        <TaskProvider>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/AddTask" element={<AddTask />} />
+            <Route path="/GeneralTasks" element={<GeneralTasks />} />
+          </Routes>
+        </TaskProvider>
       </ThemeProvider>
     </BrowserRouter>
+  );
+};
 
-  )
-}
-
-export default Rotas
+export default Rotas;
