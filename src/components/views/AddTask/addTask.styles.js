1688f1719f8aaf4c5c2c1e01494b539260f42907
laisamarcelino <laisamsr12@gmail.com>
2023-10-12 17:23:09 -0300

    update: atualizacao da pagina

---
+++ src/components/views/AddTask/addTask.styles.js
@@ -0,0 +1,71 @@
+import styled from "styled-components"
+
+export const StylesAddTask = styled.main`
+    
+    margin: 5% 10%;
+    border: solid black 2px;
+    border-radius: 12px;
+    background-color: ${(props) => props.theme.grayLight};
+    padding: 10px 35px;
+    height: fit-content;
+
+    .title {
+        display: flex;
+        justify-content: center;
+        border-bottom: 3px solid  ${(props) => props.theme.gray};
+        padding: 25px;
+        margin-bottom: 50px;
+    }
+    
+    .title-input {
+        border: none;
+        font-family: 'Montserrat', sans-serif;
+        font-size: 36px;
+        color: ${(props) => props.theme.black};
+        font: bold;
+        background-color: transparent;
+        text-align: center;
+    }
+
+    .align-datetime {
+        display: flex;
+        justify-content: space-around;
+        margin-bottom: 40px;
+    }
+
+    .align-text-input {
+        display: flex;
+        gap: 20px;
+        align-items: center;
+    }
+
+    label {
+        font-size: 20px;
+    }
+
+    .align-description {
+        display: flex;
+        gap: 20px;
+        flex-direction: column;
+    }
+
+    .input-datetime, .description {
+        border-radius: 20px;
+        padding: 15px;
+        font-size: 20px;
+        font-family: 'Montserrat', sans-serif;
+    }
+
+    .input-datetime {
+        background-color: ${(props) => props.theme.greenLight};
+        border-color: ${(props) => props.theme.jade};
+    }
+
+    .align-button {
+        margin-top: 40px;
+        display: flex;
+        justify-content: center;
+        gap: 20px;
+    }
+`
+    
