97e381aa08afb5bb8c46da07e6847e086746fa6b
laisamarcelino <laisamsr12@gmail.com>
2023-10-12 17:24:14 -0300

    update: correcao do arquivo

--- src/components/common/NewTask/NewTask.jsx
+++ src/components/common/NewTask/NewTask.jsx
@@ -1,10 +1,27 @@
 import styled from "styled-components";
+import calendar from "/calendar.svg";
+import clock from "/clock.svg";
 
-const NewTask = ({ texto, variant}) => {
+const NewTask = ({ variant, title, date, time, description}) => {
   return (
     <>
-      {variant == "task-todo" && <TodoDiv >{texto} </TodoDiv>}
-      {variant == "task-done" && <DoneDiv >{texto}</DoneDiv>}
+      {variant == "task-todo" && (
+        <TodoDiv>
+          <div>
+            {title}: {description}
+          </div>
+          <section className="align-section">
+            <div className="align-icon">
+              <img src={calendar} alt="icone de tarefas feitas, logo do site" />
+              {date}
+            </div>
+            <div className="align-icon">
+              <img src={clock} alt="icone de tarefas feitas, logo do site" />
+              {time}
+            </div>
+          </section>
+        </TodoDiv>
+      )}
     </>
   );
 };
@@ -15,20 +32,27 @@ const StyleNewTask = styled.div`
   border: solid 2px black;
   border-radius: 5px;
   padding: 10px;
-  font-size: 20px;
+  font-size: 25px;
   margin-bottom: 2%;
   font-family: "Montserrat", sans-serif;
   color: ${(props) => props.theme.black};
-  background-color: transparent;
   width: 65%;
   height: 55px;
-`;
+  align-items: center;
+  display: flex;
+  justify-content: space-around;
 
-const TodoDiv = styled(StyleNewTask)`
-  
+  .align-icon {
+    display: flex;
+    align-items: center;
+    font-size: small;
+    gap: 5px;
+  }
+
+  .align-section {
+    gap: 10px;
+  }
 `;
-const DoneDiv = styled(StyleNewTask)`
-  opacity: 0.8;
-  text-decoration: line-through;
-  width: 70%;
+const TodoDiv = styled(StyleNewTask)`
+  background-color: transparent;
 `;
