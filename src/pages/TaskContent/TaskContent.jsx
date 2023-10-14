b7c18f282a3680c3085c66ab346c12fd79dfc495
laisamarcelino <laisamsr12@gmail.com>
2023-10-14 12:43:57 -0300

    feat: inclusao de funcao para editar tarefa

--- src/pages/TaskContent/TaskContent.jsx
+++ src/pages/TaskContent/TaskContent.jsx
@@ -1,11 +1,11 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState } from "react";
 
 const TaskContext = createContext();
 
 export const useTaskContext = () => useContext(TaskContext);
 
 export function TaskProvider({ children }) {
-  const [tasks, setTasks] = useState([]); // Inicialize tasks como uma matriz vazia
+  const [tasks, setTasks] = useState([]);
 
   // Função para adicionar uma nova tarefa
   const addTask = (task) => {
@@ -29,19 +29,34 @@ export function TaskProvider({ children }) {
     setTasks(updatedTasks);
   };
 
-  // Função para atualizar uma tarefa
-  const updateTasks = (updatedTask) => {
-    const updatedTasks = tasks.map((task) => {
-      if (task.id === updatedTask.id) {
-        return updatedTask;
-      }
-      return task;
-    });
-    setTasks(updatedTasks);
+ // Função para atualizar uma tarefa
+ const updateTasks = (updatedTask) => {
+  const updatedTasks = tasks.map((task) => {
+    if (task.id === updatedTask.id) {
+      return {
+        ...task,
+        title: updatedTask.title,
+        date: updatedTask.date,
+        time: updatedTask.time,
+        description: updatedTask.description,
+      };
+    }
+    return task;
+  });
+  setTasks(updatedTasks);
+};
+
+  // Função para pesquisar tarefas
+  const searchTasks = (searchTerm) => {
+    return tasks.filter((task) =>
+      task.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
   };
 
   return (
-    <TaskContext.Provider value={{ tasks, addTask, deleteTask, doneTask, updateTasks }}>
+    <TaskContext.Provider
+      value={{ tasks, addTask, deleteTask, doneTask, updateTasks, searchTasks }}
+    >
       {children}
     </TaskContext.Provider>
   );
