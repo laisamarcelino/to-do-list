fca308130c2c66aff3bf6095c4c0024863ec35de
laisamarcelino <laisamsr12@gmail.com>
2023-10-14 10:35:49 -0300

    update: alteração de estilo da header

--- src/components/views/Header/Header.jsx
+++ src/components/views/Header/Header.jsx
@@ -1,25 +1,24 @@
-import React from 'react'
-import {StylesHeader} from '../Header/header.styles'
-import logo from '/list-checks.svg'
-import icon_search from '/magnifying-glass.svg'
+import React from "react";
+import { StylesHeader } from "../Header/header.styles";
+import logo from "/list-checks.svg";
 
 const Header = () => {
-    return (
-        <StylesHeader className={"header"}>
-            <nav className={"nav-bar"}>
-                <div className='align-logo'>
-                    <img className='logo' src={logo} alt="icone de tarefas feitas, logo do site" />
-                    <p>TO-DO LIST</p>
-                </div>
-                <div className={"search-bar"}>
-                    <form action="/">
-                        <input type="text" id="search-task" name="search-task" className={"form"} placeholder="Buscar tarefa"/> 
-                    </form>
-                    <button className='search-button'><img src={icon_search} alt="imagem de uma lupa para pesquisa" /></button>
-                </div>
-            </nav>
-        </StylesHeader>
-    )
-}
 
-export default Header
+  return (
+    <StylesHeader className={"header"}>
+      <nav className={"nav-bar"}>
+        <div className="align-logo">
+          <img
+            className="logo"
+            src={logo}
+            alt="icone de tarefas feitas, logo do site"
+          />
+          <p>TO-DO LIST</p>
+        </div>
+       
+      </nav>
+    </StylesHeader>
+  );
+};
+
+export default Header;
--- src/components/views/Header/header.styles.js
+++ src/components/views/Header/header.styles.js
@@ -22,32 +22,9 @@ export const StylesHeader = styled.header`
         font-size: 20px;
     }
 
-    .search-bar {
-        background-color: ${(props) => props.theme.white};
-        border: none;
-        border-radius: 25px;
-        padding: 20px;
-        width: 30%;
-        height: 55px;
-        
-    }
-   
-
-    .nav-bar, .search-bar {
+    .nav-bar {
         display: flex;
         justify-content: space-between;
         align-items: center;
     }
-
-    .form {
-        border: none;
-        font-family: 'Montserrat', sans-serif;
-        font-size: 15px;
-    }
-    
-    .search-button {
-        border: none;
-        background-color: ${(props) => props.theme.white};
-    }
-    
 `
