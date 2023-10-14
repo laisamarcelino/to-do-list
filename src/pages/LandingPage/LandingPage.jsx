0f396626e580aafa093d3c191dfb3b26d4d9ed78
laisamarcelino <laisamsr12@gmail.com>
2023-10-14 10:38:55 -0300

    update: insercao da componente search tasks

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Header from "../../components/views/Header/Header"
+import SearchTask from '../SearchTask/SearchTask'
 import GeneralTasks from '../GeneralTasks/GeneralTasks'
 import Footer from '../../components/views/Footer/Footer'
+
 
 
 const LandingPage = () => {
     return (
         <div>
            <Header/>
+           <SearchTask/>
            <GeneralTasks/>
            <Footer/>
         </div>
