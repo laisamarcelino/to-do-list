a8c7796497fd6ea523925e71a176fbed8e6a45d4
laisamarcelino <laisamsr12@gmail.com>
2023-10-14 12:43:07 -0300

    feat: criacao de funcao editar tarefa

--- src/pages/EditTodo/EditTodo.jsx
+++ src/pages/EditTodo/EditTodo.jsx
@@ -1,69 +1,77 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Dialog from "@mui/material/Dialog";
 import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
 import Button from "@mui/material/Button";
+import { TextField, DialogActions } from "@mui/material";
 
-const EditTaskDialog = ({ open, task, onClose, onSave, updateTasks }) => {
-  const [editedTask, setEditedTask] = useState(task ? { ...task } : { title: '', date: '', time: '', description: '' });
+const EditTaskDialog = ({ open, task, onClose, onSave }) => {
+  const [editedTask, setEditedTask] = useState(
+    task ? { ...task } : { title: "", date: "", time: "", description: "" }
+  );
+
+  useEffect(() => {
+    if (task) {
+      setEditedTask({ ...task });
+    }
+  }, [task]);
 
   const handleSave = () => {
-    // Remova a chamada para onSave aqui
-
-    // Atualize as tarefas no componente pai
-    updateTasks(editedTask); // Passe a tarefa atualizada para a função de atualização
-
+    onSave(editedTask);
     onClose();
   };
 
   return (
-    <Dialog open={open} onClose={onClose}>
-      <div>
-        <DialogTitle>Editar Tarefa</DialogTitle>
-      </div>
-        
+    <Dialog open={open} onClose={onClose} fullWidth>
+      <DialogTitle>Editar Tarefa</DialogTitle>
+
       <DialogContent>
-        <label htmlFor="titleID"> Título: </label>
-        <input
-          type="text"
-          id="titleID"
-          value={editedTask.title}
-          onChange={(e) =>
-            setEditedTask({ ...editedTask, title: e.target.value })
-          }
-        />
-        <label htmlFor="dateID"> Data: </label>
-        <input
-          type="date"
-          id="dateID"
-          value={editedTask.date}
-          onChange={(e) =>
-            setEditedTask({ ...editedTask, date: e.target.value })
-          }
-        />
-        <label htmlFor="timeID"> Horário: </label>
-        <input
-          type="time"
-          id="timeID"
-          value={editedTask.time}
-          onChange={(e) =>
-            setEditedTask({ ...editedTask, time: e.target.value })
-          }
-        />
-        <label htmlFor="descriptionID"> Descrição: </label>
-        <textarea
-          id="descriptionID"
-          placeholder="Digite algo..."
-          cols="30"
-          rows="10"
-          value={editedTask.description}
-          onChange={(e) =>
-            setEditedTask({ ...editedTask, description: e.target.value })
-          }
-        />
-        <br />
-        <Button onClick={handleSave}>Salvar</Button>
+        {editedTask && (
+          <>
+            <label htmlFor="titleID"> Título: </label>
+            <TextField
+              type="text"
+              id="titleID"
+              value={editedTask.title}
+              onChange={(e) =>
+                setEditedTask({ ...editedTask, title: e.target.value })
+              }
+            />
+            <label htmlFor="dateID"> Data: </label>
+            <TextField
+              type="date"
+              id="dateID"
+              value={editedTask.date}
+              onChange={(e) =>
+                setEditedTask({ ...editedTask, date: e.target.value })
+              }
+            />
+            <label htmlFor="timeID"> Horário: </label>
+            <TextField
+              type="time"
+              id="timeID"
+              value={editedTask.time}
+              onChange={(e) =>
+                setEditedTask({ ...editedTask, time: e.target.value })
+              }
+            />
+            <label htmlFor="descriptionID"> Descrição: </label>
+            <TextField
+              id="descriptionID"
+              placeholder="Digite algo..."
+              value={editedTask.description}
+              onChange={(e) =>
+                setEditedTask({ ...editedTask, description: e.target.value })
+              }
+            />
+          </>
+        )}
       </DialogContent>
+
+      <DialogActions>
+        <Button onClick={onClose}>Cancelar</Button>
+        <Button onClick={handleSave}>Salvar</Button>
+      </DialogActions>
     </Dialog>
   );
 };
